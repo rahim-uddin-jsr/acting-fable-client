62773bde1529ff9e3de8dbf1f2e4835364bd271c
dev-rahim <rahimuddin5915@gmail.com>
2023-06-07 11:46:28 +0600

    navigation added done

--- src/shared/Navigation/Navigation.jsx
+++ src/shared/Navigation/Navigation.jsx
@@ -1,5 +1,89 @@
+import { Link, NavLink } from "react-router-dom";
+
 const Navigation = () => {
-  return <div>nav</div>;
+  const username = true;
+  const navList = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/instructors"}>Instructors</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/classes"}>Classes</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/dashboard"}>Dashboard</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navList}
+          </ul>
+        </div>
+        <Link className="btn btn-ghost normal-case text-2xl">actingFable</Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navList}</ul>
+      </div>
+      <div className="navbar-end">
+        {!username ? (
+          <NavLink to={"/login"} className="btn">
+            Login
+          </NavLink>
+        ) : (
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://i.ibb.co/zbhLJpp/P3-OLGJ1-copy-1.png" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
