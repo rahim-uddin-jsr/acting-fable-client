77f603704ade8a3401b9dafdb28009a5bf53f376
dev-rahim <rahimuddin5915@gmail.com>
2023-06-11 14:15:18 +0600

    instractors can add classes feature added done

--- src/Pages/Dashboards/InstructorDashboard/AddClass/AddClass.jsx
+++ src/Pages/Dashboards/InstructorDashboard/AddClass/AddClass.jsx
@@ -1,16 +1,51 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 import { AuthContext } from "../../../../Context/AuthProvider/AuthProvider";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const AddClass = () => {
+  const img_token = import.meta.env.VITE_IMG_UPLOAD_TOKEN;
+  const img_api_url = `https://api.imgbb.com/1/upload?key=${img_token}`;
+  const [axiosSecure] = useAxiosSecure();
   const { user } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    const formData = new FormData();
+    formData.append("image", data.img[0]);
+    fetch(img_api_url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgRes) => {
+        const imageUrl = imgRes?.data?.display_url;
+        if (imgRes.success) {
+          data.img = imageUrl;
+          data.status = "pending";
+          console.log("upload done");
+          axiosSecure.post("/classes", data).then((data) => {
+            console.log("after posting new menu item", data.data);
+            if (data.data.insertedId) {
+              Swal.fire({
+                position: "top-center",
+                icon: "success",
+                title: "Class added successfully",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+              reset();
+            }
+          });
+        }
+      });
+  };
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-[1024px]">
@@ -31,7 +66,9 @@ const AddClass = () => {
               <div className="relative">
                 <input
                   type="text"
+                  readOnly
                   value={user?.displayName}
+                  {...register("instructorName")}
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter email"
                 />
@@ -43,8 +80,9 @@ const AddClass = () => {
               <div className="relative">
                 <input
                   type="email"
+                  readOnly
                   value={user?.email}
-                  {...register("email")}
+                  {...register("instructorEmail")}
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter email"
                 />
@@ -59,7 +97,7 @@ const AddClass = () => {
 
               <div className="relative">
                 <input
-                  {...register("title")}
+                  {...register("name")}
                   type="text"
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Class title"
@@ -72,7 +110,7 @@ const AddClass = () => {
               <div className="relative">
                 <input
                   type="number"
-                  {...register("total-sit")}
+                  {...register("availableSeats")}
                   className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Total sit"
                 />
